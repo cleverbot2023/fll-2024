3cfb9ee3f13eebb750838df3ef518680e2209dd0
ahelya <ahelyam28@rosevillecollege.com>
2024-11-03 10:14:43 +1100

    Update sensor.py

--- test/sensor.py
+++ test/sensor.py
@@ -1,1 +1,12 @@
+from hub import light_matrix
+import runloop
+import color_sensor
+from hub import port
+
+async def main():
+    # write your code here
+    test = color_sensor.color(port.E)
+    print(test)
+
+runloop.run(main())
 
