4f1712c6e55bb5352db60e24b937e6a1792e8f50
ahelya <ahelyam28@rosevillecollege.com>
2024-11-13 16:22:09 +1100

    Update 4-cargo-boat.py

--- missions/4-cargo-boat.py
+++ missions/4-cargo-boat.py
@@ -7,13 +7,15 @@ import color_sensor
 import sys
 import time
 from hub import port, motion_sensor
+from hub import light_matrix
 
 
 ################### modify code below
 async def main():
+    await light_matrix.write("4")
     motor_pair.pair(motor_pair.PAIR_1, port.A, port.B)
     motion_sensor.set_yaw_face(motion_sensor.BACK)
-   loop = True
+    loop = True
     while(loop):
         color = color_sensor.rgbi(port.E)[0]
         print(color)
